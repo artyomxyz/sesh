e63c0579d42b9f256cdc3d48bcf81c89872dd439
Sanchez486 <fergusonalexandr@mail.ru>
2015-07-14 21:49:39 +0300

    exec command development

--- src/sesh.c
+++ src/sesh.c
@@ -5,6 +5,8 @@
 #include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <stdarg.h>
+#define maxargs 255
 
 /* Declaration of tty control structures */
 struct termios _savetty;
@@ -127,7 +129,7 @@ void read_str(char* _str, char* _com) {
 		i++;
 	}
 	_com[i] = NULL;
-	puts(_com);
+	puts(_str);
 }
 
 void cmd_cd(char* _str, char* _com) { printf("This is cd\n"); };
@@ -167,15 +169,32 @@ void cmd_exec(char* _str, char* _com)
 { 
 	pid_t p;
 	p=0;
-	char* argv[]={sizeof(char[80]),0};
-	char home[82]="./";
-	argv[0]=strcat(home,_com);
+	char* args[maxargs];
+	char com[82]="./";
+	strcat(com,_com);
 	p=fork();
 	wait();
 	if (p==0){
-		if (execv(argv[0],&argv[0])==-1){
+		int argno=0;
+		args[0]=" ";
+		char* str,nsp;
+		strcpy(str,_str);
+		for (argno;argno<10;argno++){
+			args[argno]=" ";
+			argno++;
+			if (str=strchr(&str," ")==0){
+				if (argno==1) printf("no args sooca\n");
+				args[argno]=(char*)0;
+				break;
+			}
+			else {
+				nsp=strchr(str++," ");
+				strncpy(args[argno],&str,strlen(str)-strlen(nsp));
+			}
+		}
+		if (execv(com,args)==-1){
 		printf("No such file: ");
-		puts(argv[0]);
+		puts(com);
 		}
 		exit(-1);
 	}
