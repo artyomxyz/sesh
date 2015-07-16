c5fffef96ed3034971ab8c5dd85c9cbe0bdfdc80
Margarita52 <payk52@mail.ru>
2015-07-16 11:09:26 +0300

    -history1

--- src/history.c
+++ src/history.c
@@ -2,10 +2,10 @@
 
 #include <stdio.h>
 
-void history_cmd(char* _str, char* _com)
+FILE* history_fd;
+
+void history_init()
 {
-	char str[50];
-					
 	const char * ghp(const char * ot)
 	{
 		static char buff[256];
@@ -13,41 +13,35 @@ void history_cmd(char* _str, char* _com)
 		strcat(buff, ot);
 		return buff;
 	}
-					
-	FILE *pfile;
-	pfile=fopen("ghp('.sesh_history')","r");
-	if (pfile==NULL)
+	history_fd=fopen("ghp('.sesh_history')","a+");
+}
+
+void history_cmd(char* _str, char* _com)
+{
+	char str[50];
+	history_init();
+	if (history_fd==NULL)
 	{
 		puts("History is empty.\n");
 	}
 	else
 	{
 		printf("This is history:\n");
-		while (fgets(str,50,pfile)!=NULL)
- 		{
+		while (fgets(str,50,history_fd)!=NULL)
+		{
 			printf("%s",str);
 		}
 	}
-	fclose(pfile);
+	fclose(history_fd);
 }
 void history_save_cmd(char* _com)
 {
-	FILE *pfile;
-				
-	        const char * ghp(const char * ot)
-        {
-                static char buff[256];
-                strcpy(buff, getenv("HOME"));
-                strcat(buff, ot);
-                return buff;
-        }
-        			
-	pfile=fopen("ghp('.sesh_history')","a");
-	if (pfile==NULL)
+	history_init();
+	if (history_fd==NULL)
 	{
 		puts("Problems!\n");
 	}
-	fputs(_com,pfile);
-	fputs("\n",pfile);
-	fclose(pfile);
+	fputs(_com,history_fd);
+	fputs("\n",history_fd);
+	fclose(history_fd);
 }
