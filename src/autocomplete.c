89b9eb03a66fc6dc47f8bb49ac14d736b5f07602
kazakov24alex <kazakov24alex@gmail.com>
2015-07-16 21:13:14 +0300

    changes

--- src/autocomplete.c
+++ src/autocomplete.c
@@ -9,7 +9,7 @@
 	char* array[256];
 	char buff[8192];
 
-char** ls_cmd(int argc, char** argv) 
+char** autocomplete(int argc, char** argv) 
 {
 	struct dirent *entry;
 	char name_dir[256];
@@ -23,33 +23,25 @@ char** ls_cmd(int argc, char** argv)
    	if (!dir)
         	printf("Sorry, can' t do ls.");
 
-   	while ( (entry = readdir(dir)) != NULL)
-	{	
+   	while ( (entry = readdir(dir)) != NULL)	{	
 		int i = 0;
-		int a = 0;	
-		while((argv[1][i] != NULL))
-		{
-			if(entry->d_name[i] != argv[1][i])
-			{
-				a = 1;
+		int notPrefix = 0;	
+		while((argv[1][i] != '\0'))		{
+			if(entry->d_name[i] != argv[1][i]) {
+				notPrefix = 1;
 				break;
 			}
-			if(argv[1][i] == NULL)
-				break;
-			if(entry->d_name[i] == NULL)
- 			{
-				a = 1;
+			if(entry->d_name[i] == NULL) {
+				notPrefix = 1;
 				break;
 			}
 			i++;
 		}
-		if (a == 0)
-		{
+		if (notPrefix == 0) {
 			Wcounter++;
 			array[Wcounter] = buff+Scounter;
 			i = 0;
-			while(entry->d_name[i]!=0)
-			{
+			while(entry->d_name[i]!=0) {
 				buff[Scounter] = entry->d_name[i];
 				printf("%c\n",buff[Scounter]);
 				Scounter++;
