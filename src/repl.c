53fc2c9add274c1fcd5cceee0262c414ae16ed44
maximova136 <maximova14@gmail.com>
2015-07-20 23:35:54 +0300

    fixed

--- src/repl.c
+++ src/repl.c
@@ -17,25 +17,31 @@
 #define KEY_SC_RIGHT 'C'
 #define KEY_SC_LEFT 'D'
 
-char buff[1024], buffcpy[1024];
-char *cur;
+unsigned char buff[1024], buffcpy[1024];
+unsigned char *cur;
 
-void replace_buf(char *cmd_name){
-	//printf("[%d]\n", cur-buff);
-	//char *cmd_cur=cmd_name;
-	while(cur > buff) {
-		write(STDOUT_FILENO, "\b \b", 3);
+void erase_char(){
+	write(STDOUT_FILENO, "\b \b", 3);
+	cur--;
+	if (*cur >= 128) {
 		cur--;
 	}
-	//cur++;
+}
+
+void replace_buf(unsigned char *cmd_name) {
+	//printf("[%d]\n", cur-buff);
+	//char *cmd_cur=cmd_name;
+	while (cur > buff) {
+		erase_char();
+	}
 	while (*cmd_name != '\n'){
 		*(cur++)=*(cmd_name++);
 	}
-	*(cur)='\0';
+	*(cur) = '\0';
 	write(STDOUT_FILENO, buff, strlen(buff));
 }
 
-int parse_args(char* buff, char** argv) {
+int parse_args(unsigned char* buff, char** argv) {
 	int argc = 0;
 
 	while (1) {
@@ -66,11 +72,12 @@ int parse_args(char* buff, char** argv) {
 		}
 	}
 
-	printf("%d\n", argc);
+	//printf("%d\n", argc);
 
 	argv[argc] = NULL;
 	return argc;
 }
+
 
 /* Description:
 *	REPL = Read, Evaluate, Print, Loop	
@@ -82,45 +89,43 @@ int parse_args(char* buff, char** argv) {
 */
 
 void repl () {
-	char typecom[][8] = { "cd", "history", "help", "ls" };
+	unsigned char typecom[][8] = { "cd", "history", "help", "ls" };
 	void(*arr_func[])(int, char**) = { dir_cmd, history_cmd, help_cmd, ls_cmd};
 	
 	while (1) {
 		// Prompt
-		char cwd[256];
+		unsigned char cwd[256];
 		getcwd(cwd, 256);
 		// printf("%s > ", cwd);
 		write(STDOUT_FILENO, cwd, strlen(cwd));
-		write(STDOUT_FILENO, " > ", 3);
-		
+		write(STDOUT_FILENO, " > ", 3);		
 		
 		// Read
 		cur = buff;
-		char c[2];
-
+		unsigned char c[2];
 		
-		char *com_name=NULL;
-		int h_count=0;
+		unsigned char *com_name = NULL;
+		int h_count = 0;
 
 		while (read(STDIN_FILENO, &c, 1) != 0) {
 			if (c[0] == '\n') {
-				h_count=0;
+				h_count = 0;
 				break;
 			}
 			switch(c[0]) {
 				case KEY_BACKSPACE: 
 				case KEY_DELETE:
 					if (cur == buff) break;
-					write(STDOUT_FILENO, "\b \b", 3);
-					cur--;
+					erase_char();		
 					break;
+					
 				case KEY_ARROWS:
 					read(STDIN_FILENO, &c, 2);
 					if (c[0] == '['){
 						switch(c[1]){
 							case KEY_SC_UP:
 								//write(STDOUT_FILENO, "up", 2);
-								com_name=history_entry(h_count);
+								com_name = history_entry(h_count);
 								if (com_name!=NULL){
 									replace_buf(com_name);
 									//write(STDOUT_FILENO,com_name,strlen(com_name));	
@@ -132,7 +137,7 @@ void repl () {
 									//write(STDOUT_FILENO,"fubar",5);	
 									replace_buf("");
 								} else {
-									com_name=history_entry(h_count);
+									com_name = history_entry(h_count);
 									if (com_name!=NULL){
 										replace_buf(com_name);
 										//write(STDOUT_FILENO,com_name,strlen(com_name));
@@ -141,14 +146,15 @@ void repl () {
 								}
 								break;
 							case KEY_SC_RIGHT:
-								write(STDOUT_FILENO, "right", 5);
+								//write(STDOUT_FILENO, "right", 5);
 								break;
 							case KEY_SC_LEFT:
-								write(STDOUT_FILENO, "left", 4);
+								//write(STDOUT_FILENO, "left", 4);
 								break;
 							     }
 							}
 					break;
+					
 				case KEY_TAB:
 					write(STDOUT_FILENO, "	", 1);
 					break;
